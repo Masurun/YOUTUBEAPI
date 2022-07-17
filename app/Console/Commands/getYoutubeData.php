744f4bd75a4d85602080cf97dbdd7f73c20667c0
Masurun <wataru.5756@gmail.com>
2022-07-18 06:45:05 +0900

    Initial commit

---
+++ app/Console/Commands/getYoutubeData.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use Google_Client;
+use Google_Service_YouTube;
+use Google_Service_Exception;
+use Google_Exception;
+
+class getYoutubeData extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'command:get_youtube_data';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        $apikey = '';
+        $client = new Google_Client();
+        $client->setApplicationName("youtube-api-test");
+        $client->setDeveloperKey($apikey);
+        $youtube = new Google_Service_YouTube($client);
+
+
+        //動画の取得
+        // $params['channelId'] = '';
+        // $params['type'] = 'video';
+        // $params['maxResults'] = 10;
+        // $params['order'] = 'date';
+        // try {
+        //     $searchResponse = $youtube->search->listSearch('snippet', $params);
+        // } catch (Google_Service_Exception $e) {
+        //     echo htmlspecialchars($e->getMessage());
+        //     exit;
+        // } catch (Google_Exception $e) {
+        //     echo htmlspecialchars($e->getMessage());
+        //     exit;
+        // }
+        // foreach ($searchResponse['items'] as $search_result) {
+        //     $videos[] = $search_result;
+        // }
+
+        $res = fopen('', 'w');
+        $header = ["日付", "ユーザー名", "コメント","返信数","いいね"];
+        fputcsv($res, $header);
+        $nextPageToken = NULL;
+        while(true){
+            $comments = $youtube->commentThreads->listCommentThreads('snippet,replies', array(
+                'videoId' => '',
+                'textFormat' => 'plainText',
+                'pageToken' => $nextPageToken,
+                'maxResults' => 100,
+            ));
+            for ($i = 0; $i < 100; $i++) {
+                $arr = array();
+                $arr[] = $comments[$i]['snippet']['topLevelComment']['snippet']['updatedAt'];
+                $arr[] = $comments[$i]['snippet']['topLevelComment']['snippet']['authorDisplayName'];
+                $arr[] = $comments[$i]['snippet']['topLevelComment']['snippet']['textDisplay'];
+                $arr[] = $comments[$i]['snippet']['topLevelComment']['snippet']['totalReplyCount'];
+                $arr[] = $comments[$i]['snippet']['topLevelComment']['snippet']['likeCount'];
+                fputcsv($res, $arr);
+            }
+            $nextPageToken = $comments['nextPageToken'];
+            if($nextPageToken==NULL){
+                break;
+            }
+        }
+        //文字化け対策　未実装
+        // mb_convert_variables('SJIS', 'UTF-8', $res);
+        fclose($res);
+    }
+}
